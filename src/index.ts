972fae48c5b57019b237b0bfa248e59b48da7c43
JesusBMX <jjbaezmm@hotmail.com>
2025-05-27 17:27:01 -0600

    release

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import DB, { type QueryResult, type SqlRequest } from "./DB";
 import ItMigration from "./ItMigration";
 import Model from "./Model";
-import QueryBuilder, { type QueryOptions, type Where} from "./QueryBuilder";
+import QueryBuilder, { type QueryOptions, type WhereClause, type OrderBy, type JoinClause} from "./QueryBuilder";
 import Schema, { Column, Constraint, Index, TableSchema } from "./Schema"
 
 export type {
   SqlRequest,
-  Where,
+  JoinClause,
+  WhereClause,
+  OrderBy,
   QueryOptions,
   QueryResult
 }
